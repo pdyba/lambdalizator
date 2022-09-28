f24d8d095abdd7cc19b1e1d279c3a9ad7da7687a
Piotr Dyba <piotr@dyba.it>
2022-09-28 18:11:30 +0200

    changes from CR

--- tests/test_lambdas/test_enums.py
+++ tests/test_lambdas/test_enums.py
@@ -1,5 +1,3 @@
-from typing import Callable
-
 import pytest
 
 from lbz.lambdas.enums import LambdaSource
@@ -16,19 +14,19 @@ from tests.exemplary_events import (
 @pytest.mark.parametrize(
     "source_event, expected_type",
     (
-        (api_gw_event, LambdaSource.API_GW),
-        (direct_lambda_event, LambdaSource.DIRECT),
-        (dynamodb_event, LambdaSource.DYNAMODB),
-        (event_bridge_event, LambdaSource.EVENT_BRIDGE),
-        (s3_event, LambdaSource.S3),
-        (sqs_event, LambdaSource.SQS),
+        (api_gw_event(), LambdaSource.API_GW),
+        (direct_lambda_event(), LambdaSource.DIRECT),
+        (dynamodb_event(), LambdaSource.DYNAMODB),
+        (event_bridge_event(), LambdaSource.EVENT_BRIDGE),
+        (s3_event(), LambdaSource.S3),
+        (sqs_event(), LambdaSource.SQS),
     ),
 )
-def test_lambda_source_gives_correct_type_per_full_event(
-    source_event: Callable, expected_type: str
+def test__lambda_source__correctly_recognizes_source_based_on_provided_event_full(
+    source_event: dict, expected_type: str
 ) -> None:
-    assert LambdaSource.get_source(source_event()) == expected_type
-    assert LambdaSource.is_from(source_event(), expected_type)
+    assert LambdaSource.get_source(source_event) == expected_type
+    assert LambdaSource.is_from(source_event, expected_type)
 
 
 @pytest.mark.parametrize(
@@ -42,15 +40,15 @@ def test_lambda_source_gives_correct_type_per_full_event(
         ({"invoke_type": "direct_lambda_request"}, LambdaSource.DIRECT),
     ],
 )
-def test_event_types_minimal(source_event: dict, expected_type: str) -> None:
+def test__lambda_source__correctly_recognizes_source_based_on_provided_event_minimal(source_event: dict, expected_type: str) -> None:
     assert LambdaSource.get_source(source_event) == expected_type
     assert LambdaSource.is_from(source_event, expected_type)
     assert not LambdaSource.is_from(source_event, "covid")
 
 
-def test_event_types_raises_not_implemented() -> None:
+def test__lambda_source__raises_error_when_event_cannot_be_recognized() -> None:
     event = {"Records": [{"eventSource": "aws:covid"}]}
     with pytest.raises(NotImplementedError, match="Unsupported event type: covid"):
-        assert LambdaSource.get_source(event) == "covid"
+        LambdaSource.get_source(event)
     with pytest.raises(NotImplementedError, match="Unsupported event type: covid"):
-        assert LambdaSource.is_from(event, "covid")
+        LambdaSource.is_from(event, "covid")
