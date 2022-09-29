1e2bf2d419ae31abf15ead57c8c686a247e60a83
Piotr Dyba <piotr@dyba.it>
2022-09-29 09:23:57 +0200

    adds missing tests

--- tests/test_lambdas/test_broker.py
+++ tests/test_lambdas/test_broker.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock
 
+from lbz.exceptions import NotFound
 from lbz.lambdas import LambdaBroker, LambdaResult
 
 
@@ -38,4 +39,27 @@ class TestEventBroker:
         assert resp == {
             "result": LambdaResult.BAD_REQUEST,
             "message": "Lambda execution error: Missing 'op' field in the event.",
+        }
+
+    def test_broker_handles_expected_error_outcome(self) -> None:
+        def func_1(_event: dict) -> None:
+            raise NotFound()
+
+        mapper = {"x": func_1}
+        event = {"op": "x", "data": {"y": 1}}
+        resp = LambdaBroker(mapper, event).react()  # type: ignore
+
+        assert resp == {"result": LambdaResult.SERVER_ERROR, "message": NotFound.message}
+
+    def test_broker_handles_unexpected_error_outcome(self) -> None:
+        def func_1(_event: dict) -> None:
+            raise EOFError("oops")
+
+        mapper = {"x": func_1}
+        event = {"op": "x", "data": {"y": 1}}
+        resp = LambdaBroker(mapper, event).react()  # type: ignore
+
+        assert resp == {
+            "result": LambdaResult.SERVER_ERROR,
+            "message": "EOFError('oops')",
         }
