f73a28c33082f1f3c4af191475401070d1f91f1e
Piotr Dyba <piotr@dyba.com.pl>
2022-10-01 11:06:32 +0200

    adds base handler and lambda api (#68)
    * adds base handler and lambda api
    
    * adds missing requriements and updates  setup.py
    
    * adds missing requriements and updates  setup.py
    
    * Apply suggestions from code review
    
    Co-authored-by: Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
    
    * changes from Code Review
    
    * fixes from discusion
    
    * merge
    
    * adds missing tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
    
    * changes from CR
    
    * changes from CR
    
    * Apply suggestions from code review
    
    Co-authored-by: Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
    
    * changes from CR
    
    Co-authored-by: Piotr Dyba <piotr@dyba.it>
    Co-authored-by: Grzegorz Redlicki <redlicki.grzegorz@gmail.com>

---
+++ tests/test_handlers.py
@@ -0,0 +1,52 @@
+import logging
+from unittest.mock import MagicMock, patch
+
+import pytest
+from pytest import LogCaptureFixture
+
+from lbz.handlers import BaseHandler
+
+
+class MyBaseHandler(BaseHandler):
+    def handle(self) -> str:
+        return "something"
+
+
+@patch.object(MyBaseHandler, "post_handle", autospec=True)
+@patch.object(MyBaseHandler, "pre_handle", autospec=True)
+def test__react__triggers_both_pre_and_post_handle(
+    pre_handle: MagicMock, post_handle: MagicMock
+) -> None:
+    response = MyBaseHandler().react()
+
+    assert response == "something"
+    pre_handle.assert_called_once()
+    post_handle.assert_called_once()
+
+
+@patch.object(MyBaseHandler, "post_handle", autospec=True)
+@patch.object(MyBaseHandler, "pre_handle", autospec=True)
+def test__react__raises_error_when_pre_handle_fails(
+    pre_handle: MagicMock, post_handle: MagicMock
+) -> None:
+    pre_handle.side_effect = ValueError
+
+    with pytest.raises(ValueError):
+        MyBaseHandler().react()
+
+    pre_handle.assert_called_once()
+    post_handle.assert_not_called()
+
+
+@patch.object(MyBaseHandler, "post_handle", autospec=True)
+def test__react__only_logs_error_when_post_handle_fails(
+    post_handle: MagicMock, caplog: LogCaptureFixture
+) -> None:
+    post_handle.side_effect = ValueError("xxxx")
+
+    response = MyBaseHandler().react()
+
+    assert response == "something"
+    post_handle.assert_called_once()
+
+    assert caplog.record_tuples == [("lbz.handlers", logging.ERROR, "xxxx")]
