aea3b5ad4bcac244fa86701df4b56ae8e939eb19
Piotr Dyba <piotr@dyba.it>
2022-10-06 18:15:26 +0200

    temp fix for AWS region

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,9 @@
+import os
+
 from tests.fixtures.rsa_pair import (  # noqa:F401
     EXPECTED_TOKEN,
     SAMPLE_PRIVATE_KEY,
     SAMPLE_PUBLIC_KEY,
 )
+
+os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
