4e0ff97615833dc3005ab809fccbd93dcb432899
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2023-12-30 13:37:21 +0100

    Leave implementing the hash method for the future

--- lbz/events/event.py
+++ lbz/events/event.py
@@ -2,7 +2,8 @@ import json
 from typing import Optional
 
 
-class Event:
+# TODO: Implement the hash method to stay compatible with pylint requirements
+class Event:  # pylint: disable=eq-without-hash
     type: str
 
     def __init__(self, data: dict, *, event_type: Optional[str] = None) -> None:
