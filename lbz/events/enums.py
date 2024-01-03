164344c7732559e521526a629ad8466bd5080b35
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2024-01-03 16:06:45 +0100

    Resolve all the problems pointed out by the newly added bandit library

--- lbz/events/enums.py
+++ lbz/events/enums.py
@@ -2,7 +2,7 @@ class CognitoEventType:
     # https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-custom-email-sender.html
     EMAIL_ACCOUNT_TAKE_OVER_NOTIFICATION = "CustomEmailSender_AccountTakeOverNotification"
     EMAIL_ADMIN_CREATE_USER = "CustomEmailSender_AdminCreateUser"
-    EMAIL_FORGOT_PASSWORD = "CustomEmailSender_ForgotPassword"
+    EMAIL_FORGOT_PASSWORD = "CustomEmailSender_ForgotPassword"  # nosec B105
     EMAIL_RESEND_CODE = "CustomEmailSender_ResendCode"
     EMAIL_SIGNUP = "CustomEmailSender_SignUp"
     EMAIL_UPDATE_USER_ATTRIBUTE = "CustomEmailSender_UpdateUserAttribute"
@@ -11,7 +11,7 @@ class CognitoEventType:
     # https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-custom-sms-sender.html
     SMS_ADMIN_CREATE_USER = "CustomSMSSender_AdminCreateUser"
     SMS_AUTHENTICATION = "CustomSMSSender_Authentication"
-    SMS_FORGOT_PASSWORD = "CustomSMSSender_ForgotPassword"
+    SMS_FORGOT_PASSWORD = "CustomSMSSender_ForgotPassword"  # nosec B105
     SMS_RESEND_CODE = "CustomSMSSender_ResendCode"
     SMS_SIGNUP = "CustomSMSSender_SignUp"
     SMS_UPDATE_USER_ATTRIBUTE = "CustomSMSSender_UpdateUserAttribute"
