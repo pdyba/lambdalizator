a9497075c15fdcd401b84d516eacfdca25c343f0
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2025-11-23 09:23:50 +0100

    Add the newest versions of Python to keep the library up-to-date

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.10",
