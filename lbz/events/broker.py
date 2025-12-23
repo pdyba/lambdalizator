3afb7fe0474770758ae8d3cac83fcb21981267bf
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2025-12-23 14:58:09 +0100

    Add the ability to implement what should happen when the handler fails

--- lbz/events/broker.py
+++ lbz/events/broker.py
@@ -28,15 +28,18 @@ class BaseEventBroker(BaseBroker[None]):
     def handlers(self) -> list[EventHandler]:
         try:
             return self.mapper[self.event.type]
-        except KeyError as err:
-            raise NotImplementedError(f"No handlers implemented for {self.event.type}") from err
+        except KeyError as error:
+            raise NotImplementedError(f"No handlers implemented for {self.event.type}") from error
 
     def handle(self) -> None:
         for handler in self.handlers:
             try:
                 handler(deepcopy(self.event))
-            except Exception:  # pylint: disable=broad-except
-                logger.exception("Handling event failed, event: %s", self.event)
+            except Exception as error:  # pylint: disable=broad-except
+                self.on_error(error)
+
+    def on_error(self, error: Exception) -> None:  # pylint: disable=unused-argument
+        logger.exception("Handling event failed, event: %s", self.event)
 
     def pre_handle(self) -> None:
         pass
