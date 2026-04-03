7db971acd6ee3d06e837732602cae7bbd20d1b9f
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2026-04-03 12:28:58 +0200

    Do not log an extra warning when the Content-Type is not JSON-compatible

--- lbz/request.py
+++ lbz/request.py
@@ -8,10 +8,8 @@ from multidict import CIMultiDict
 
 from lbz.authentication import User
 from lbz.exceptions import BadRequestError
-from lbz.misc import MultiDict, get_logger
+from lbz.misc import MultiDict
 from lbz.rest import ContentType
-
-logger = get_logger(__name__)
 
 
 class Request:
@@ -73,15 +71,14 @@ class Request:
         if self._json_body is None:
             content_type: str | None = self.headers.get("Content-Type")
             if content_type is None:  # pylint: disable=consider-using-assignment-expr
-                return None
-            if content_type.startswith(ContentType.JSON):
+                self._json_body = None
+            elif content_type.startswith(ContentType.JSON):
                 if isinstance(self.raw_body, dict) or self.raw_body is None:
                     self._json_body = self.raw_body
                 else:
                     self._json_body = self._safe_json_loads(self.raw_body)
             else:
-                logger.warning("Wrong headers: %s", self.headers)
-                raise BadRequestError(f"Content-Type header is missing or wrong: {content_type}")
+                raise BadRequestError(f"Content-Type is not JSON-compatible: {content_type}")
         return self._json_body
 
     def to_dict(self) -> dict:
